04f5116fefa7ca0992bec0dd1b10e888856848a2
jumin20099 <jumin20099@gmail.com>
2024-03-25 11:55:20 +0900

    예약 페이지 추가

--- pages/sub03.php
+++ pages/sub03.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="ko">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>skills baseball park</title>
+    <link rel="stylesheet" href="./style.css" >
 </head>
+
 <body>
-    
+    <?php include ("./components/header.php") ?>
+    <?php include("./components/footer.php") ?>
 </body>
+
 </html>
