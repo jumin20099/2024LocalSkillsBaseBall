e6d66dfee3b301704e5f0625e219004a3cc91462
jumin20099 <jumin20099@gmail.com>
2024-03-25 11:54:26 +0900

    라우터 추가

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-include("./api/DBconnect.php");
+include ("./api/DBconnect.php");
 
 $request = $_SERVER['REQUEST_URI'];
 $path = explode("?", $request);
-$path[1] = isset($path[1]) ? $path[1] : null;
+$path[1] = isset ($path[1]) ? $path[1] : null;
 $resource = explode("/", $path[0]);
 
 $pages = "";
@@ -23,9 +23,15 @@ switch ($resource[1]) {
     case 'goods':
         $pages = './pages/sub04.php';
         break;
+    case 'signup':
+        $pages = './pages/signup.php';
+        break;
+    case 'signin':
+        $pages = './pages/signin.php';
+        break;
     default:
         echo "ㄴㄴ";
         return 0;
 }
-include($pages);
+include ($pages);
 ?>
