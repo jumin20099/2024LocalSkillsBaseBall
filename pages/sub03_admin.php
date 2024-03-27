2a34ad20d0ad64f6dfd0db592847cda810130c3d
jumin20099 <jumin20099@gmail.com>
2024-03-28 03:41:23 +0900

    sub03_admin 페이지 코드를 sub03_manger 페이지로 이동

--- pages/sub03_admin.php
+++ pages/sub03_admin.php
@@ -1,47 +1,3 @@
-<?php
-$userSql = "SELECT * FROM reservation WHERE is_deleted = 0 ORDER BY reservated_date DESC";
-$resSql = "SELECT * FROM user";
-
-$stmt = $pdo->prepare($userSql);
-$stmt = $pdo->prepare($resSql);
-$stmt->execute();
-
-$reservations = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>
-
-<div id="reservationContainer">
-    <?php
-    if ($reservations) {
-        foreach ($reservations as $reservation) {
-            $userSql = "SELECT * FROM user WHERE user_idx = :user_idx";
-            $stmt = $pdo->prepare($userSql);
-            $stmt->bindParam(":user_idx", $reservation["user_idx"]);
-            $stmt->execute();
-            $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            $reservationUser_idx = isset ($reservation["user_idx"]) ? $reservation["user_idx"] : '';
-
-            // 예약 목록은 체크박스, 예약자ID, 예약자 이름, 리그, 날짜, 시간, 최소인원, 사용료, 예약가능여부, 예약승인버튼, 삭제버튼이 있다
-            // 이즈 레저베이티드 = 불리언 말고 이넘 3가지
-            // 결제 상태도 추가 , 이넘3
-            $divInnerText = "";
-            $divInnerText .= "<div class='post'>";
-            $divInnerText .= "<p class='id'> 예약자 ID : " . $user["username"] . "</p>";
-            $divInnerText .= "<p class='id'> 예약자 이름 : " . $user["name"] . "</p>";
-            $divInnerText .= "<p class='id'> 리그 : " . $reservation["league"] . "</p>";
-            $divInnerText .= "<p class='id'> 날짜 : " . $reservation["reservated_date"] . "</p>";
-            $divInnerText .= "<p class='id'> 시간 : " . $reservation["game_time"] . "</p>";
-            $divInnerText .= "<p class='id'> 최소인원 : " . $reservation["min_user"] . "</p>";
-            $divInnerText .= "<p class='id'> 사용료 : " . $reservation["price"] . "</p>";
-            $divInnerText .= "<p class='id'> 예약가능여부 : " . $reservation["is_reservated"] . "</p>";
-            $divInnerText .= "</div>";
-            $divInnerText .= "<br>";
-            echo $divInnerText;
-        }
-    }
-    ?>
-</div>
 <!DOCTYPE html>
 <html lang="ko">
 
@@ -56,9 +12,9 @@ $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
 </head>
 
 <body>
-    <?php include ("./components/header.php") ?>
+    <?php include("./components/header.php") ?>
 
-    <?php include ("./components/footer.php") ?>
+    <?php include("./components/footer.php") ?>
 
     <script src="./선수제공파일/bootstrap-5.2.0-dist/js/bootstrap.js"></script>
     <script src="./script.js"></script>
