493167a357403fa9045ceac539bd4eebf62e8945
jumin20099 <jumin20099@gmail.com>
2024-04-16 08:44:21 +0900

    admin 휴일 지정 용 게임 테이블 추가

--- pages/sub03_admin.php
+++ pages/sub03_admin.php
@@ -107,6 +107,115 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <body>
     <!-- <?php include("./components/header.php") ?> -->
+    
+    <article id="gameTable">
+        <table id="resTable">
+            <tr>
+                <th>일</th>
+                <th>월</th>
+                <th>화</th>
+                <th>수</th>
+                <th>목</th>
+                <th>금</th>
+                <th>토</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td onclick="reservationModal(this)">1</td>
+                <td onclick="reservationModal(this)">2</td>
+                <td onclick="reservationModal(this)">3</td>
+                <td onclick="reservationModal(this)">4</td>
+                <td onclick="reservationModal(this)">5</td>
+                <td onclick="reservationModal(this)">6</td>
+            </tr>
+            <tr>
+                <td onclick="reservationModal(this)">7</td>
+                <td onclick="reservationModal(this)">8</td>
+                <td onclick="reservationModal(this)">9</td>
+                <td onclick="reservationModal(this)">10</td>
+                <td onclick="reservationModal(this)">11</td>
+                <td onclick="reservationModal(this)">12</td>
+                <td onclick="reservationModal(this)">13</td>
+            </tr>
+            <tr>
+                <td onclick="reservationModal(this)">14</td>
+                <td onclick="reservationModal(this)">15</td>
+                <td onclick="reservationModal(this)">16</td>
+                <td onclick="reservationModal(this)">17</td>
+                <td onclick="reservationModal(this)">18</td>
+                <td onclick="reservationModal(this)">19</td>
+                <td onclick="reservationModal(this)">20</td>
+            </tr>
+            <tr>
+                <td onclick="reservationModal(this)">21</td>
+                <td onclick="reservationModal(this)">22</td>
+                <td onclick="reservationModal(this)">23</td>
+                <td onclick="reservationModal(this)">24</td>
+                <td onclick="reservationModal(this)">25</td>
+                <td onclick="reservationModal(this)">26</td>
+                <td onclick="reservationModal(this)">27</td>
+            </tr>
+            <tr>
+                <td onclick="reservationModal(this)">28</td>
+                <td onclick="reservationModal(this)">29</td>
+                <td onclick="reservationModal(this)">30</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </article>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form id="reservationForm" action="" method="POST">
+                        <label for="league">리그 선택 : </label>
+                        <select id="league" name="league"
+                            onchange="feeCalculator(this, document.getElementById('minPlayers'))">
+                            <option value="나이트리그">나이트리그</option>
+                            <option value="주말리그">주말리그</option>
+                            <option value="새벽리그">새벽리그</option>
+                        </select><br>
+                        <div id="selectedDate">
+
+                        </div>
+                        <label for="time">시간 : </label>
+                        <select id="reservationTime" name="time">
+                            <option id="firstGame" name="time">19시</option>
+                            <option id="secondGame" name="time">23시</option>
+                            <option id="thirdGame" name="time">15시</option>
+                        </select><br>
+                        <label style="display:none;" for="players">최소인원 : </label>
+                        <input style="display:none;" onchange="feeCalculator(document.getElementById('league'), this)" type="number" id="minPlayers" name="minPlayers" value="20" min="20"><br>
+                        <input type="hidden" id="selectedDateInput" name="selectedDate" value="" />
+                        <input type="hidden" id="totalPriceInput" name="totalPrice" value="" />
+                        <div style="display:none;" id="feeCalculateResult">
+
+                        </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
+                    <?php
+                    if (isset ($_SESSION["user_idx"])) {
+                        echo "<button type='submit' class='btn btn-primary'>예약하기</button>";
+                    } else {
+                        echo "<button type='submit' disabled class='btn btn-primary'>로그인 후 예약 가능합니다</button>";
+                    }
+                    ?>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
     <?php include("./components/footer.php") ?>
     <script src="./선수제공파일/bootstrap-5.2.0-dist/js/bootstrap.js"></script>
     <script src="./script.js"></script>
